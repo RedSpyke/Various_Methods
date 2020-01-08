0cfcb44f27dee9a8c2c66b1739543c0526c655b0
Alexandru Palcau <alexandrupalcau@outlook.com>
2020-01-08 16:42:05 +0200

    Various Methods add new methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,13 @@
 package com.company;
 
+import java.lang.invoke.SwitchPoint;
+
 public class Main {
 
     public static void main(String[] args) {
-System.out.println(getGreatestCommonDivisor(12,30));
+      numberToWords(1010);
+      System.out.println(getDigitCount(0));
+
 
     }
 
@@ -94,5 +98,132 @@ System.out.println(getGreatestCommonDivisor(12,30));
         return gcd;
     }
 
+    public static void printFactors (int number){
+        if (number < 0){
+            System.out.println("Invalid Value");
+        }
+        for (int i = 1 ; i <= number ; i++){
+            if (number % i == 0){
+                System.out.println(i);
+            }
+        }
+
+    }
+
+    public static boolean  isPerfectNumber(int number){
+        if (number < 1 ){
+            return false;
+        }
+        int numbersSum = 0;
+
+        for (int i = 1; i < number; i++){
+            if (number % i != 0){
+               continue;
+            }
+            numbersSum = numbersSum + i;
+             if (numbersSum == number){
+                 return true;
+             }
+        }
+        return false;
+
+    }
+
+    public static void numberToWords (int number){
+        if (number <0){
+            System.out.println("Invalid Value");
+        }
+        int numberOfDigits = getDigitCount(number);
+        String Zero = "Zero ";
+         // System.out.println(numberOfDigits);
+        int reverseNumber = reverse(number);
+        int lastDigitCounter = 0;
+        String NumberToWord = " ";
+        while (reverseNumber != 0){
+            int lastDigit = reverseNumber % 10;
+
+            String lastDigitWord = " ";
+            switch (lastDigit){
+                case 0:
+                    lastDigitWord = "Zero";
+                    break;
+                case 1:
+                    lastDigitWord = "One";
+                    break;
+                case 2:
+                    lastDigitWord = "Two";
+                    break;
+                case 3:
+                    lastDigitWord = "Three";
+                    break;
+                case 4:
+                    lastDigitWord = "Four";
+                    break;
+                case 5:
+                    lastDigitWord = "Five";
+                    break;
+                case 6:
+                    lastDigitWord = "Six";
+                    break;
+                case 7:
+                    lastDigitWord = "Seven";
+                    break;
+                case 8:
+                    lastDigitWord = "Eight";
+                    break;
+                case 9:
+                    lastDigitWord = "Nine";
+                    break;
+                default:
+                    break;
+            }
+            NumberToWord += lastDigitWord + ' ';
+            reverseNumber /=10;
+            lastDigitCounter++;
+        }
+        // System.out.println(lastDigitCounter);
+        // System.out.println(NumberToWord);
+        if (numberOfDigits != lastDigitCounter){
+            int n = numberOfDigits - lastDigitCounter;
+            for (int i = 0; i < n; i++ ){
+                NumberToWord += Zero;
+            }
+        }
+        System.out.println(NumberToWord);
+    }
+
+    public static int reverse (int number){
+        int originalNumber = number;
+        if (number < 0){
+            number *= -1;
+        }
+        int reverseNumber = 0;
+        while (number != 0){
+
+            int lastDigit = number % 10;
+            reverseNumber =reverseNumber * 10 + lastDigit;
+            number /=10;
+        }
+
+        if (originalNumber < 0){
+            reverseNumber *= - 1;
+        }
+        return reverseNumber;
+    }
+
+public static int getDigitCount (int number){
+        if (number < 0){
+            return -1;
+        }
+        if (number <= 9){
+            return 1;
+        }
+        int digits = 0;
+    while (number != 0){
+        number /=10;
+        digits++;
+    }
+        return digits;
+}
 
 }
